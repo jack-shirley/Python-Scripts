c3a906493a9f41eb17e6d6746624c418f97101cb
Luke Kiowski <lkiowski@gmail.com>
2019-07-02 22:55:20 -0500

    Added variables to clean up script

--- Weather_Bot/weatherBot.py
+++ Weather_Bot/weatherBot.py
@@ -6,18 +6,34 @@ import webbrowser
 import pyowm
 import smtplib
 
-owm = pyowm.OWM('***API KEY***') #valide api key w/ server
+api_key = '***API_KEY***'
+owm = pyowm.OWM(api_key) #valide api key w/ server
 
+# Customize these variables
+from_email = '***FROM_EMAIL***'
+password = '***PASSWORD***'
+to_email = '***TO_EMAIL***'
 location = 'Fort Worth, USA' #Change to anything you want
 
+# Retrieves the weather from the API
 observation = owm.weather_at_place(location)
 w = observation.get_weather()
-print(w.get_temperature('fahrenheit')["temp_max"])#Grabs only the temperature
 
-x = w.get_temperature('fahrenheit')["temp_max"]
+# Gets the main temperatures for the day
+temp = w.get_temperature('fahrenheit')["temp"]
+temp_max = w.get_temperature('fahrenheit')["temp_max"]
+temp_min = w.get_temperature('fahrenheit')["temp_min"]
 
+print('Temperature in ' + location + ': ' + str(temp)) # Display the temperature
+
+# Subject and message
+subject = 'Subject: Weather Report.\n'
+message = 'Hello, the weather is as follows.\n\n\
+It is currently ' + str(temp) + ' in ' + location + '\n\n\
+Have a wonderful day.'
+
+# Start the SMTP server and send message
 smtpObj = smtplib.SMTP('smtp.gmail.com', 587)
-type(smtpObj)
 smtpObj.starttls()
-smtpObj.login('***EMAIL***', '***PASSWORD***')
-smtpObj.sendmail('***FROM_EMAIL***', '***TO_EMAIL***', 'Subject: Weather Report.\nHello, the weather today is as follows.\n\nIt is currently ' + str(x) + ' in ' + location + '\n\nHave a wonderful day.')
+smtpObj.login(from_email, password)
+smtpObj.sendmail(from_email, to_email, subject + message)
